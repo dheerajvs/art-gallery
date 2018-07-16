b4493681337bf3ced4cd5284d1827da807d0fc68
Dheeraj Vepakomma <dheeraj.vepakomma@evry.com>
2018-07-16 21:41:41 +0530

    feat: index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Button from '@material/react-button'
+import TopAppBar from '@material/react-top-app-bar'
+import MaterialIcon from '@material/react-material-icon'
 
-import Header from './header'
 import './layout.css'
 
 const Layout = ({ children, data }) => (
@@ -28,19 +28,23 @@ const Layout = ({ children, data }) => (
             { name: 'keywords', content: 'sample, something' },
           ]}
         />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <nav>
-          <Button>Gallery</Button>
-          <Link to="/about/">About</Link>
-          <Link to="/contact/">Contact</Link>
-        </nav>
+        <TopAppBar
+          title={data.site.siteMetadata.title}
+          actionItems={[
+            <MaterialIcon icon='home' />,
+            <MaterialIcon icon='info' />,
+            <MaterialIcon icon='contact_support' />,
+          ]}
+        />
         <div
           style={{
             margin: '0 auto',
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            paddingLeft: '1.0875rem',
+            paddingRight: '1.0875rem',
+            paddingBottom: ' 1.45rem',
           }}
+          className="mdc-top-app-bar--fixed-adjust"
         >
           {children}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
+import Button from '@material/react-button'
 
 import Layout from '../components/layout'
 
+import './index.css'
+
 const IndexPage = ({ data }) => {
-  const { edges: items } = data.allMarkdownRemark
+  const { edges } = data.allMarkdownRemark
+  const categories = edges.filter(
+    ({ node }) => node.frontmatter.templateKey === 'category'
+  )
+  const items = edges.filter(
+    ({ node }) => node.frontmatter.templateKey === 'item'
+  )
 
   return (
     <Layout>
-      {items.map(({ node: item }) => (
-        <div key={item.fields.slug}>
-          <Link to={item.fields.slug}>{item.frontmatter.title}</Link>
-        </div>
+      {categories.map(({ node: categoryNode }) => (
+        <section key={categoryNode.fields.slug}>
+          <header>
+            <Button>{categoryNode.frontmatter.title} ▸</Button>
+          </header>
+          <ul>
+            {items.filter(({ node: itemNode }) => (
+              itemNode.frontmatter.categories.find(({ category }) =>
+                category === categoryNode.frontmatter.title
+              )
+            )).map(({ node: itemNode }) => (
+              <li>
+                <Link to={itemNode.fields.slug}>{itemNode.frontmatter.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </section>
       ))}
     </Layout>
   )
@@ -30,16 +52,18 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark(
-      filter: { frontmatter: { templateKey: { eq: "item" } }}
-    ) {
+    allMarkdownRemark {
       edges {
         node {
           fields {
             slug
           }
           frontmatter {
+            templateKey
             title
+            categories {
+              category
+            }
           }
         }
       }

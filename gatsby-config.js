72ebf18540461d22baaa70677a20328ae58e122f
Dheeraj Vepakomma <dheeraj.vepakomma@evry.com>
2018-07-19 23:01:14 +0530

    fix: use gatsby plugins for sass and styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {

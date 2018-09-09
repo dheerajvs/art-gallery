62298b80acd48bc6e79931e70fed79228f40e5cd
Dheeraj Vepakomma <dheeraj.vepakomma@evry.com>
2018-09-10 00:14:45 +0530

    about page

--- src/templates/item/item.js
+++ src/templates/item/item.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 

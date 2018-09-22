bc211bca969e2dc6978ba2c8da6b8d5fc0ba4426
Dheeraj Vepakomma <dheeraj.vepakomma@evry.com>
2018-09-22 21:49:41 +0530

    manifest configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,18 @@ module.exports = {
         plugins: [],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sowmya Art Gallery`,
+        short_name: `Art Gallery`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#8b4513`,
+        display: `standalone`,
+        icon: `static/img/favicon.png`,
+      },
+    },
     // 'gatsby-plugin-netlify-cms',
     // 'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],

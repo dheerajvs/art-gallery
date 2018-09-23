f53547b21372dd1b757dfee07b75bb4180c42255
Dheeraj Vepakomma <dheeraj.vepakomma@evry.com>
2018-09-23 13:52:16 +0530

    social share buttons

--- src/templates/item.js
+++ src/templates/item.js
@@ -7,13 +7,25 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import HelpIcon from '@material-ui/icons/Help'
+import ShareIcon from '@material-ui/icons/Share'
+import EmailIcon from '@material-ui/icons/Email'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faTwitter,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
 
 import withRoot from 'withRoot'
 import Layout from 'components/layout'
 
-const styles = () => ({
+const styles = theme => ({
   cardContainer: {
     maxWidth: 640
   },
@@ -25,66 +37,189 @@ const styles = () => ({
     maxWidth: '100%',
     maxHeight: 'calc(100vh - 116px)',
   },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+  facebook: {
+    color: '#3b5998'
+  },
+  twitter: {
+    color: '#55acee'
+  },
+  whatsapp: {
+    color: '#25d366'
+  },
+  email: {
+    color: '#777'
+  },
 })
 
 function handleEnquireClick() {
   navigate('/contact/')
 }
 
-const Item = ({ classes, data }) => {
-  const { frontmatter: item } = data.markdownRemark
+class Item extends React.Component {
 
-  return (
-    <Layout titlePrefix={item.title}>
-      <Grid container justify="center" spacing={32}>
-        <Grid item>
-          <img
-            className={classes.image} src={item.large_image} alt={item.title}
-          />
-        </Grid>
-        <Grid className={classes.cardContainer} item>
-          <Card>
-            <CardContent>
-              <Typography variant="headline">{item.title}</Typography>
-              <Typography variant="subheading" paragraph>
-                {item.medium}
-              </Typography>
-              <Typography variant="body1" paragraph>
-                {item.description}
-              </Typography>
-              <Typography variant="body1" paragraph>
-                {`${item.width} \xD7 ${item.height} inches`}
-                {` (${Math.round(item.width * 2.54)}`}
-                {` \xD7 ${Math.round(item.height * 2.54)} cm)`}
-              </Typography>
-              {item.sold && (
-                <Typography variant="body2" component="p">
-                  &#x1F3F7;&#xFE0F; {
-                    `Sold out ${
-                      item.available ? '(available as commissioned work)' : ''
-                  }`
-                }</Typography>
-              ) || (
-                <Typography
-                  className={classes.available} variant="body2" component="p"
-                >
-                  Available.
+  state = {
+    anchorEl: null
+  }
+
+  handleShareMenuClose = app => {
+    const { title } = this.props.data.markdownRemark.frontmatter
+    const text = `Take a look at ${title} from Sowmya\u2019s Art Gallery`
+
+    this.setState({ anchorEl: null })
+
+    switch (app) {
+      case 'facebook':
+        window.open(
+          `https://facebook.com/sharer/sharer.php?u=${location.href}&t=${text}`
+        )
+        break
+      case 'twitter':
+        window.open(
+          `https://twitter.com/intent/tweet/?text=${text}&url=${location.href}`
+        )
+        break
+      case 'whatsapp':
+        window.open(`whatsapp://send?text=${text}%20${location.href}`)
+        break
+      case 'email':
+        window.open(`mailto:?subject=${text}&body=${location.href}`)
+        break
+      default:
+          break
+    }
+  }
+
+  handleShareClick = event => {
+    if (navigator.share) {
+      const { title } = this.this.props.data.markdownRemark.frontmatter
+
+      navigator.share({
+        title: 'Sowmya\u2019s Art Gallery',
+        text: `Take a look at ${title} from Sowmya\u2019s Art Gallery`,
+        url: location.href,
+      })
+        .catch(() => {});
+    }
+    else {
+      this.setState({ anchorEl: event.currentTarget })
+    }
+  }
+
+  render() {
+    const { classes, data } = this.props
+    const { frontmatter: item } = data.markdownRemark
+    const { anchorEl } = this.state
+
+    return (
+      <Layout titlePrefix={item.title}>
+        <Grid container justify="center" spacing={32}>
+          <Grid item>
+            <img
+              className={classes.image} src={item.large_image} alt={item.title}
+            />
+          </Grid>
+          <Grid className={classes.cardContainer} item>
+            <Card>
+              <CardContent>
+                <Typography variant="headline">{item.title}</Typography>
+                <Typography variant="subheading" paragraph>
+                  {item.medium}
                 </Typography>
-              )}
-            </CardContent>
-            <CardActions>
-              <Button
-                size="small" color="primary" onClick={handleEnquireClick}
-              >
-                Enquire
-              </Button>
-              <Button size="small" color="primary">Share</Button>
-            </CardActions>
-          </Card>
+                <Typography variant="body1" paragraph>
+                  {item.description}
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  {`${item.width} \xD7 ${item.height} inches`}
+                  {` (${Math.round(item.width * 2.54)}`}
+                  {` \xD7 ${Math.round(item.height * 2.54)} cm)`}
+                </Typography>
+                {item.sold && (
+                  <Typography variant="body2" component="p">
+                    &#x1F3F7;&#xFE0F; {
+                      `Sold out ${
+                        item.available ? '(available as commissioned work)' : ''
+                    }`
+                  }</Typography>
+                ) || (
+                  <Typography
+                    className={classes.available} variant="body2" component="p"
+                  >
+                    Available.
+                  </Typography>
+                )}
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small" color="primary" onClick={handleEnquireClick}
+                >
+                  <HelpIcon
+                    className={classNames(classes.leftIcon, classes.iconSmall)}
+                  />
+                  Enquire
+                </Button>
+                <Button
+                  size="small" color="primary"
+                  onClick={this.handleShareClick}
+                  aria-owns={anchorEl ? 'share-menu' : null}
+                  aria-haspopup="true"
+                >
+                  <ShareIcon
+                    className={classNames(classes.leftIcon, classes.iconSmall)}
+                  />
+                  Share
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </Layout>
-  )
+        <Menu
+          id="share-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleShareMenuClose}
+        >
+          <MenuItem onClick={() => this.handleShareMenuClose('facebook')}>
+            <ListItemIcon
+              className={classNames(classes.iconSmall, classes.facebook)}
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </ListItemIcon>
+            Facebook
+          </MenuItem>
+          <MenuItem onClick={() => this.handleShareMenuClose('twitter')}>
+            <ListItemIcon
+              className={classNames(classes.iconSmall, classes.twitter)}
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </ListItemIcon>
+            Twitter
+          </MenuItem>
+          <MenuItem onClick={() => this.handleShareMenuClose('whatsapp')}>
+            <ListItemIcon
+              className={classNames(classes.iconSmall, classes.whatsapp)}
+            >
+              <FontAwesomeIcon icon={faWhatsapp} color="green" />
+            </ListItemIcon>
+            WhatsApp
+          </MenuItem>
+          <MenuItem onClick={() => this.handleShareMenuClose('email')}>
+            <ListItemIcon
+              className={classNames(classes.iconSmall, classes.email)}
+            >
+              <EmailIcon />
+            </ListItemIcon>
+            Email
+          </MenuItem>
+        </Menu>
+      </Layout>
+    )
+  }
 }
 
 Item.propTypes = {
